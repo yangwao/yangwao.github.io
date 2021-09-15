21d2ca4da02e7724531612a87ec04893fc72fd55
Matej Nemček <ybdaba@gmail.com>
2021-09-15 11:25:37 +0200

    added investment thesis

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  title: 'Life path of Wao',
+  title: 'Life path of yangWao',
   description: 'Long time no see, life reflection',
   // plugins: ['clean-urls', {
   //   normalSuffix: '.html',
   //   indexSuffix: '/'}],
   theme: 'simple',
   themeConfig: {
-    author: 'Matej Nemček',
+    author: 'Matej Yangwao',
     navbar: {
+      investment_thesis: '/investment_thesis',
       experience: '/experience',
       work: '/work',
       production: '/production',
